953b4a8705b85d4cdb8aeba0e56b4bf46e026746
yen-shi <yenshiw@andrew.cmu.edu>
2019-10-05 15:08:00 -0400

    Add run.sh for running all of the benchmarks

---
+++ code/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p log
+
+for i in $(seq 1 91);
+do
+  echo "Running benchmark #${i}"
+  timeout 10 bash -c "echo \"S ${i}\" | ./RS > log/result_${i}.log 2>&1";
+  RETVAL=$?
+  if [ "$RETVAL" -eq "124" ]; then
+    echo "Benchmark #${i} timeout"
+  fi
+done
+
