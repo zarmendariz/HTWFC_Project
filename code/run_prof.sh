29d3ccbc7b38dc1ee8f58078d0415950ded30214
yen-shi <yenshiw@andrew.cmu.edu>
2019-10-06 02:54:17 -0400

    Modify run.sh and add run_prof.sh for profiling

--- code/run_prof.sh
+++ code/run_prof.sh
@@ -2,29 +2,32 @@
 
 if [ "$#" -ne 1 ];
 then
-  echo "./run.sh <time_limit>";
+  echo "./run_prof.sh <time_limit>";
   exit 0;
 fi
 
 COUNT=0
 TIME_LIMIT=${1}
-BIN=./RS_O1
+BIN=./RS_PG
 
 if [ ! -f "${BIN}" ]; then
   echo "Executable '${BIN}' does not exist.";
   exit 0;
 fi
 
-mkdir -p log
+mkdir -p profile
 
 for i in $(seq 1 91);
 do
   echo "Running benchmark #${i}"
-  timeout ${TIME_LIMIT} bash -c "echo \"S ${i}\" | ${BIN} > log/result_${i}.log 2>&1";
+  echo "S ${i}" > tmp
+  echo "Q" >> tmp
+  timeout ${TIME_LIMIT} bash -c "cat tmp | ${BIN}";
   RETVAL=$?
   if [ "$RETVAL" -eq "124" ]; then
     echo " >>> Benchmark #${i} timeout";
   else
+    gprof ${BIN} > profile/profile_${i}.txt
     count=$((count + 1));
   fi
 done
