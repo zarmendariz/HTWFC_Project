7fbe271fbbd5bbcc4894899886f568c8b7263478
yen-shi <danny8508021@gmail.com>
2019-10-23 23:55:28 -0400

    Clean up and add comment to run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
+##
+# run RS on a list of puzzles
+# given the indices of puzzles in LIST_OF_PUZZLES
+# output log will dump to LOG_DIR
+##
+
+LIST_OF_PUZZLES=experiments/can_solve.txt
+LOG_DIR=experiments/log
+TIME_LIMIT=${1}
+COUNT=0
+BIN=./RS_O0
+
+# the first argument is the time limit to run
 if [ "$#" -ne 1 ];
 then
   echo "./run.sh <time_limit>";
   exit 0;
 fi
 
-COUNT=0
-TIME_LIMIT=${1}
-BIN=./RS_O0
-
+# check existence of binary
 if [ ! -f "${BIN}" ]; then
   echo "Executable '${BIN}' does not exist.";
   exit 0;
 fi
 
-mkdir -p log
+# check existence of output log dir
+mkdir -p ${LOG_DIR}
 
-for i in $(cat can_solve.txt);
+# run RS binary on each puzzle listed in LIST_OF_PUZZLES
+for i in $(cat ${LIST_OF_PUZZLES});
 do
   echo "Running benchmark #${i}"
-  { time timeout ${TIME_LIMIT} bash -c "echo \"S ${i}\" | ${BIN} > log/result_${i}.log 2>&1"; } >> log/result_${i}.log 2>&1;
+  { time timeout ${TIME_LIMIT} bash -c "echo \"S ${i}\" | ${BIN} > ${LOG_DIR}/result_${i}.log 2>&1"; } >> ${LOG_DIR}/result_${i}.log 2>&1;
+
   RETVAL=$?
   if [ "$RETVAL" -eq "124" ]; then
     echo " >>> Benchmark #${i} timeout";
-  else
+  elif [ "$RETVAL" -eq "0" ]; then
     count=$((count + 1));
   fi
 done
--- code/run_ith.sh
+++ code/run_ith.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+##
+# run RS on a specific puzzle
+# take one arguemnt which is the index of the puzzle
+##
+
+BIN=./RS_O0
+
 if [ "$#" -ne 1 ];
 then
   echo "./run.sh <ith benchmark>";
   exit 0;
 fi
 
-COUNT=0
-TIME_LIMIT=${1}
-BIN=./RS
-
 if [ ! -f "${BIN}" ]; then
   echo "Executable '${BIN}' does not exist.";
   exit 0;
 fi
-
-mkdir -p log
 
 i=$1;
 echo "Running benchmark #${i}"
